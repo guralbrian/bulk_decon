9395757fa5b87aa1b94c3c59728bca64f74108bc
Brian Gural <bgural@c1416ood03.ll.unc.edu>
2023-04-05 15:46:27 -0400

    Added 2_gtex_de_genes.R, a script to find non-DE genes between subject-matched pseudo bulk (snRNAseq) and bulk. Output saves a list of "stable" genes
    Renamed gtex_load.R to fit order-of-operations syntex

---
+++ scripts/general/2_gtex_de_genes.R
@@ -0,0 +1,32 @@
+# Pseudobulk-bulk DE filtering on GTEx samples
+# Compare subject matched bulk RNAseq to pseudobulk single-nucleus (sn) RNAseq
+
+# load libraries 
+library(dplyr) # load first since we'll need dplyr::lapply()
+libs <- c("Seurat", "DESeq2", "SeuratDisk") # list libraries here
+lapply(libs, require, character.only = T)
+
+source("scripts/functions/decon_all.R") # load our own functions
+
+# load data
+gtex.sn <- LoadH5Seurat("data/processed/internal/sn_gtex_lv_match.h5seurat")
+gtex.bk <- read.csv("data/processed/internal/gtex_lv_counts_summed.csv", check.names = F, row.names = 1)
+
+# keep only overlapping subjects
+gtex.bk.sub <- gtex.bk[,levels(gtex.sn$Participant.ID)]
+rownames(gtex.bk.sub) <- gtex.bk$gene
+
+# find DE genes
+good.genes <- FilterBulkSingleNucleus(gtex.sn, 
+                                     gtex.bk.sub,
+                                     min.rna.ft = 200,
+                                     max.rna.ft = 3000,
+                                     min.rna.ct = 800,
+                                     max.mt.pt = 0.01,
+                                     max.rb.pt = 0.01,
+                                     scrublet_score = 0.4,
+                                     group = "Participant.ID",
+                                     lfc.thresh = log2(5),
+                                     change = "greaterAbs") 
+
+write.csv(good.genes, "data/processed/internal/gtex_stable_genes.csv", row.names = F)
