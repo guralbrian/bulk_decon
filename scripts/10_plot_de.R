cee4acb1097167ee2620ab2e8b18fe38662b050a
Brian Gural <guralbrian@gmail.com>
2024-01-16 12:57:32 -0500

    Seperated DESeq2 script, removed PCA portion. Moved visualization to next script, which is also (in progress) in this commit

---
+++ scripts/10_plot_de.R
@@ -0,0 +1,44 @@
+# Visualize DE 
+
+libs <- c("tidyverse", "wesanderson", "ggrepel") # list libraries here
+lapply(libs, require, character.only = T)
+rm(libs)
+
+# Save the results 
+comparison.clr <- read.csv("data/processed/models/adjusted_de.csv")
+pal <- wes_palette(name = "Zissou1", type = "continuous")
+
+# Find limits for color scale/legend
+limit <- comparison.clr |> 
+  na.omit() |>
+  arrange(desc(abs(p_diff))) |>
+  slice_head(n = 1) |>
+  pull(p_diff) * c(-1, 1)
+
+plot.clr <- ggplot(comparison.clr, aes(x = log2FoldChange.clr, y = -log10(padj.clr), color = p_diff)) +
+  geom_point(alpha = 1, size = 6) +
+  scale_color_distiller(type = "div", palette = "Spectral" ) +
+  geom_text_repel(data = top.clr, aes(label = gene), size = 12, box.padding = unit(0.35, "lines"), 
+                  force = 20, segment.linetype = 2, segment.size = 0.6, color = "black", force_pull = 0.01, min.segment.length = 0) + # label most sig genes 
+  theme_minimal() +
+  labs(x = "log2(Fold Change)", y = "-log10(adjusted p-value)", color = "Change in -log10(p-value)") +
+  geom_hline(yintercept = -log10(0.05), linetype = "dashed") + 
+  geom_vline(xintercept = 0, linetype = "solid") + # add a line for p=0.05
+  theme(legend.position = "bottom",
+        axis.text.x = element_text(color = "black", size = 25),
+        axis.text.y = element_text(color = "black", size = 25),
+        axis.ticks = element_blank(),
+        title = element_text(size = 25),
+        legend.text = element_text(size = 25),
+        legend.key.size = unit(0.7, 'in'),
+        legend.title = element_text(size = 28, vjust = 0.7),
+        axis.title.y = element_text(color = "black", size = 28),
+        axis.title.x = element_text(color = "black", size = 28),
+        panel.background = element_rect(color="black"),
+        plot.background = element_rect(color="black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.margin = unit(c(1,1,1,1), "cm")) #+
+#ggtitle("DE w/ clr-based composition in model", subtitle = "Colored text show genes that became or lost significance, top 5 by change")# move legend to the bottom
+
+plot.clr
---
+++ scripts/9_differential_expression.R
@@ -0,0 +1,118 @@
+# Test for differential gene expression with DESeq2 
+# Include composition covariates to test if they mediate DE
+
+# Load libs
+libs <- c("tidyverse", "compositions", "reshape2", "stats", "DESeq2", "wesanderson", "ggrepel") # list libraries here
+lapply(libs, require, character.only = T)
+rm(libs)
+
+#### Loading and formatting of data ####
+# Load compositions, expression, and phenotype data
+decon.whole <- read.csv("data/processed/compositions/whole_samples.csv")
+phenotypes.real <- read.csv("data/processed/bulk/jensen_pheno.csv")
+jensen.bulk <- read.csv("data/processed/bulk/jensen_bulk_clean.csv",  row.names = 1, check.names = F)
+
+
+# Unmelt for clr transformation 
+decon.wide <- decon.whole  |> 
+  select(Sub, CellType, Prop) |> 
+  pivot_wider(names_from = "Sub", values_from = "Prop") |> 
+  column_to_rownames("CellType") %>%
+  mutate_all(as.numeric)
+
+# use clr or ilr to incorp the compositions into the design matrix
+comps.clr <- compositions::clr(t(decon.wide))
+
+colnames(comps.clr) <- paste0("clr.", colnames(comps.clr))
+
+# Prepare for DESeq2
+bulk <- mutate_all(jensen.bulk, function(x) round(as.numeric(as.character(x)), digits = 0)) # round to integers
+
+# Set reference factor levels for phenotypes
+pheno.reorder <- phenotypes.real |> 
+  mutate(Sub = as.factor(de_id)) |> 
+  mutate(Genotype = factor(case_when(
+    str_detect(Sub, "WT") ~ "WT",
+    str_detect(Sub, "KO") ~ "KO")),
+    Treatment = factor(case_when(
+      str_detect(Sub, "sham") ~ "Sham",
+      str_detect(Sub, "MI") ~ "TAC")))
+
+# Prepare sample information
+sample_info <- data.frame(
+  row.names = colnames(bulk),
+  genotype = as.factor(pheno.reorder$Genotype),
+  treatment = as.factor(pheno.reorder$Treatment)
+)
+
+sample_info$treatment <- relevel(sample_info$treatment, ref = "Sham")
+sample_info$genotype <- relevel(sample_info$genotype, ref = "KO")
+
+# Add clr transforms to sample info
+sample.clr <- cbind(sample_info, comps.clr)
+
+#### Run DESeq2 ####
+## DESeq with compositions
+# Create a DESeqDataSet
+dds.clr <- DESeqDataSetFromMatrix(
+  countData = bulk,
+  colData = sample.clr,
+  design = ~ treatment + genotype + treatment:genotype + clr.Cardiomyocytes + clr.Fibroblast
+)
+
+# Run DESeq
+dds.clr <- DESeq(dds.clr)
+
+# Pull out interaction term results
+res.clr <- results(dds.clr, name="treatmentTAC.genotypeWT")
+
+## DESeq without compositions
+# Create a DESeqDataSet
+dds.raw <- DESeqDataSetFromMatrix(
+  countData = bulk,
+  colData = sample_info,
+  design = ~ treatment + genotype + treatment:genotype
+)
+
+# Run DESeq 
+dds.raw <- DESeq(dds.raw)
+
+# Pull out interaction term results
+res.raw <- results(dds.raw, name="treatmentTAC.genotypeWT")
+
+#### Contrast DE results between clr and raw/unadjusted runs ####
+# Compare results
+comparison.clr <- merge(as.data.frame(res.clr), as.data.frame(res.raw), by = "row.names", suffixes = c(".clr", ".raw"))
+colnames(comparison.clr)[1] <- "gene"
+# Calculate difference in -log10 p-values
+comparison.clr$p_diff <- -log10(comparison.clr$padj.clr) -  # low numbers mean that its not signficant when clr is used
+  -log10(comparison.clr$padj.raw)    # large numbers mean that its significant w/o comp
+
+# Make categorical column
+comparison.clr <- comparison.clr |>
+  mutate(clr.sig = ifelse(padj.clr <= 0.05 & padj.raw >= 0.05, T, F),
+         raw.sig = ifelse(padj.clr >= 0.05 & padj.raw <= 0.05, T, F),
+         both.sig = ifelse(padj.clr <= 0.05 & padj.raw <= 0.05, T, F)) 
+
+# Get the gene names for the top 5 genes by change in adjusted p value, within lost and gained sig groups
+gained.clr <- comparison.clr |>  
+  subset(clr.sig == T) |>
+  arrange(desc(abs(p_diff))) |>
+  slice_head(n = 5)
+
+lost.clr <- comparison.clr |>  
+  subset(raw.sig == T) |> 
+  arrange(desc(abs(p_diff))) |>
+  slice_head(n = 5)
+
+top.clr <- comparison.clr |> 
+  arrange(padj.clr) |>
+  slice_head(n = 5)
+
+top.lost.raw <- comparison.clr |>
+  subset(raw.sig == T) |> 
+  arrange(padj.raw) |>
+  slice_head(n = 5)
+
+# Save the results 
+write.csv(comparison.clr, "data/processed/models/adjusted_de.csv", row.names = F)
